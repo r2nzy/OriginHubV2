782fdd4a9984e7e636c5229af64b2493cb474499
r2nzy <ohatt2900@gmail.com>
2024-05-16 18:23:38 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 11177482306 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/yxllxws/OriginWarePublic/main/StreetWarz2.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/r2nzy/OriginHubV2/main/SW2.lua"))()
 else
   game.Players.LocalPlayer:Kick('Origin Hub is not supported on this game yet.')
 end
