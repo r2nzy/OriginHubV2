92c01dc2cec4aa0627e03872494492f1792abd1a
r2nzy <ohatt2900@gmail.com>
2024-07-22 16:04:51 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,7 +4,7 @@ elseif game.PlaceId == 17428592195 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/r2nzy/OriginHubV2/main/BRONX%20DISTRICT.lua"))()
 elseif game.PlaceId == 10179538382 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/r2nzy/OriginHubV2/main/SB.lua"))()
-elseif game.PlaceId == then
+elseif game.PlaceId == 4779613061 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/r2nzy/OriginHubV2/main/SouthLondon2.lua"))()
 else
   game.Players.LocalPlayer:Kick('Origin Hub is not supported on this game yet.')
