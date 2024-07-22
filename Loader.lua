4d2c8c81fe65ad3c12c27b784846a837e646ab74
r2nzy <ohatt2900@gmail.com>
2024-07-22 16:13:20 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -6,6 +6,4 @@ elseif game.PlaceId == 10179538382 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/r2nzy/OriginHubV2/main/SB.lua"))()
 elseif game.PlaceId == 4779613061 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/r2nzy/OriginHubV2/main/SouthLondon2.lua"))()
-else
-  game.Players.LocalPlayer:Kick('Origin Hub is not supported on this game yet.')
 end
